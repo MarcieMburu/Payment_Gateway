b47cc6f6ff8eca87e96ec8d874a0869293ff3994
MarcieMburu <mburumercy2510@gmail.com>
2023-07-17 10:44:21 +0300

    seed data

---
+++ Payment_Gateway/Models/SeedData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Payment_Gateway.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Payment_Gateway.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Payment_GatewayContext(
+                serviceProvider.GetRequiredService<
+                       DbContextOptions<Payment_GatewayContext>>()))
+            {
+              
+                if (context.Sender.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Sender.AddRange(
+                    new Sender
+                    {
+                        Sen_Name = "Mercy",
+                        Sen_ID_NO = 87654321,
+                        Sen_Phone = 0787654321,
+                        Sen_Src_Acc = "Mpesa",
+
+
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Payment_Gateway/Program.cs
+++ Payment_Gateway/Program.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Payment_Gateway.Data;
+using Payment_Gateway.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Payment_GatewayContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Payment_GatewayContext") ?? throw new InvalidOperationException("Connection string 'Payment_GatewayContext' not found.")));
+  options.UseSqlServer(builder.Configuration.GetConnectionString("Payment_GatewayContext") ?? throw new InvalidOperationException("Connection string 'Payment_GatewayContext' not found.")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
